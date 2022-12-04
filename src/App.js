9520dea006e0f070394df7a8a5289e429b974ed8
thomas ni <857840959@QQ.COM>
2022-12-05 09:04:15 +1100

    change hello wrold

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <input type="file"></input>
-        <p>hello world</p>
+        <p>hello thomas</p>
         <a
           className="App-link"
           href="https://reactjs.org"
